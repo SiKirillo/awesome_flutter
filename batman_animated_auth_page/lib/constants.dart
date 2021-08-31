f03279419c3395ed243b71dd34adcaf92d71082e
Samarychev Kirill <samarychev98@mail.ru>
2021-08-31 12:09:54 +0300

    Patch #5
    - background color hotfix

--- batman_animated_auth_page/lib/constants.dart
+++ batman_animated_auth_page/lib/constants.dart
@@ -4,3 +4,4 @@ const kPrimaryColor = Color(0xFF1c1f26);
 const kSecondaryColor = Color(0xFF616161);
 const kPrimaryButtonColor = Color(0xFFFDE85A);
 const kSecondaryButtonColor = Color(0xFFC8B853);
+const kBatmanColor = Color(0xFF0e0e0b);
